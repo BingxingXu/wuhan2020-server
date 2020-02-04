ccc4caac9cdf036b57de629581f03bee5aaa845c
徐冰星 <xubingxing@minx.lan>
2020-02-04 19:15:54 +0800

    更新

--- src/weixin/weixin.controller.ts
+++ src/weixin/weixin.controller.ts
@@ -1,27 +1,46 @@
 import { Controller, Get, HttpService, OnModuleInit, Req, Request, Query } from '@nestjs/common';
-import { ApiConfig, ApiConfigKit, WeChat } from 'tnwx';
+import { createHash } from 'crypto';
 
 const APPID = 'wx9ed52ed8359d7f25';
 const APPSECRET = '0676cb20d3176bf5afe8523ce51cd049';
 const Token = 'test';
-const config = new ApiConfig(APPID, APPSECRET, Token, false);
+const sha1 = (v: string) => {
+    const hash = createHash('sha1');
+    hash.update(v);
+    return hash.digest('hex');
+};
+
+interface Token {
+    access_token: string;
+    expires_in?: number;
+    errcode?: number;
+    errmsg?: string;
+}
+
+interface Ticket {
+    ticket: string;
+    errcode: number;
+}
 
 @Controller('weixin')
-export class WeixinController implements OnModuleInit {
+export class WeixinController {
     constructor(private readonly httpService: HttpService) { }
-
-    onModuleInit() {
-        ApiConfigKit.putApiConfig(config);
-        ApiConfigKit.devMode = true;
-    }
 
     @Get('config')
     async getToken() {
         try {
+            const { data: tokenData } = await this.httpService.get(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${APPID}&secret=${APPSECRET}`).toPromise();
+            if ((tokenData as Token).errcode) {
+                throw new Error(tokenData.errcode);
+            }
+            const { data: ticketData } = await this.httpService.get(`https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${tokenData.access_token}&type=jsapi`).toPromise();
             const timestamp = new Date().getTime() / 1000;
             const nonceStr = 'random';
             const url = 'https://wuhan.90hub.com/';
-            const signature = await WeChat.jssdkSignature(nonceStr, `${timestamp}`, url);
+            const ticket = (ticketData as Ticket).ticket;
+            let signature = `jsapi_ticket=${ticket}&noncestr=${nonceStr}&timestamp=${timestamp}&url=${url}`;
+            signature = sha1(signature);
+
             return {
                 appId: APPID,
                 timestamp,
